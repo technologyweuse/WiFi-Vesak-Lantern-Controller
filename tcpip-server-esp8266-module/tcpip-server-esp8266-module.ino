bf19a310c0ae8131b33e12f382ff014de5e9d5da
TechnologyWeUse.Com <technologyweuse@users.noreply.github.com>
2018-04-26 17:12:01 +0900

    Update tcpip-server-esp8266-module.ino

--- tcpip-server-esp8266-module/tcpip-server-esp8266-module.ino
+++ tcpip-server-esp8266-module/tcpip-server-esp8266-module.ino
@@ -14,7 +14,7 @@
  *   GNU Lesser General Public License for more details.                    *
  *                                                                          *
  *   You should have received a copy of the GNU Lesser General Public       *
- *   License along with Self Drive Car.                                     *
+ *   License along with Vesak Lantern Control System.                       *
  *   If not, see <http://www.gnu.org/licenses/>.                            *
  *                                                                          *
  *   For detail tutorial about linked list, see                             *
